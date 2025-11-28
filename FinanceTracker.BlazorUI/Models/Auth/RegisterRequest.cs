792f78618acdaccae531c40cee90a7eaeb306edf
Vladyslav Bilyk <bilyk.vlad16@gmail.com>
2025-11-28 02:38:25 +0200

    fix: add passwoed validation on frontend model

--- FinanceTracker.BlazorUI/Models/Auth/RegisterRequest.cs
+++ FinanceTracker.BlazorUI/Models/Auth/RegisterRequest.cs
@@ -1,7 +1,15 @@
-﻿namespace FinanceTracker.BlazorUI.Models.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceTracker.BlazorUI.Models.Auth;
 
 public class RegisterRequest
 {
+    [Required(ErrorMessage = "Username is required")]
     public string UserName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(6, ErrorMessage = $"Password must be at least 6 characters")]
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).*$",
+        ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter and one digit")]
     public string Password { get; set; } = string.Empty;
 }
